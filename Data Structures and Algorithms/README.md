d92b15e2f3314dfb8576a294c784dcd3a3364c3f
amusi <amusi1994@gmail.com>
2018-05-28 00:08:16 +0800

    添加冒泡排序代码

--- Data Structures and Algorithms/README.md
+++ Data Structures and Algorithms/README.md
@@ -1,8 +1,12 @@
 # 数据结构与算法
 
-## 排序
+## 排序算法
 
-- 冒泡排序（Bubble Sort）
+![常用排序算法](http://7xsnb0.com1.z0.glb.clouddn.com/2016-07-15_%E5%B8%B8%E7%94%A8%E6%8E%92%E5%BA%8F%E7%AE%97%E6%B3%95.png)
+
+
+
+- [冒泡排序（Bubble Sort）](bubb;e_sort.cpp)
 
 
 
---
+++ Data Structures and Algorithms/bubble_sort.cpp
@@ -0,0 +1,78 @@
+/* Summary: 冒泡排序
+* Author: Amusi
+* Date:   208-05-27
+*
+* Reference: 
+*	http://en.wikipedia.org/wiki/Bubble_sort
+*	https://github.com/xtaci/algorithms/blob/master/include/bubble_sort.h
+*   https://www.zhihu.com/people/amusi1994/activities
+*
+* 冒泡排序说明：待完成
+*
+*/
+
+#include <iostream>
+
+// 冒泡函数
+namespace alg{
+	template<typename T>
+	static void BubbleSort(T list[], int length)
+	{
+
+#if 1
+		// 版本1：两层for循环 
+		for (int i = 0; i < length-1; ++i)
+		{ 
+			for (int j = 0; j < length - i -1; ++j)
+			{
+				// 两两相邻元素比较大小，从小到大排序
+				// if (list[j] < list[j + 1]) : 从大到小排序
+				if (list[j] > list[j + 1])
+				{
+					int temp = list[j + 1];
+					list[j + 1] = list[j];
+					list[j] = temp;
+				}
+			}
+		}
+
+#else
+		// 版本2：while+一层for循环
+		bool swapped = false;
+		while (!swapped)
+		{
+			swapped = true;
+	
+			for (int i = 0; i < length - 1; ++i)
+			{
+				// 两两相邻元素比较大小，从小到大排序
+				// if (list[j] < list[j + 1]) : 从大到小排序
+				if (list[i] > list[i + 1])
+				{
+					int temp = list[i + 1];
+					list[i + 1] = list[i];
+					list[i] = temp;
+				}
+				swapped = false;
+			}
+			length--;
+		}
+			
+#endif
+
+	}
+}
+
+using namespace std;
+using namespace alg;
+
+
+int main()
+{
+	int a[8] = { 5, 2, 5, 7, 1, -3, 99, 56 };
+	BubbleSort<int>(a, 8);
+	for (auto e : a)
+		std::cout << e << " ";
+
+	return 0;
+}
